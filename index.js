6cc77892c3b16401d7729b30721296ff6c0101c1
Tom Paavolainen <tuomas.paavolainen@metropolia.fi>
2021-01-06 17:51:15 +0200

    uibuild

--- index.js
+++ index.js
@@ -5,10 +5,9 @@ const cors = require("cors");
 const Person = require("./models/person");
 const app = express();
 
+app.use(cors());
 app.use(express.static("build"));
 app.use(express.json());
-app.use(express.urlencoded());
-app.use(cors());
 
 const unknownEndpoint = (req, res) => {
   res.status(404).send({ error: "unknown endpoint" });
