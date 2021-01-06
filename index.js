52b51a2131bba2fe0a683e3ce21d5adb89a52f87
Tom Paavolainen <tuomas.paavolainen@metropolia.fi>
2021-01-06 11:46:49 +0200

    uibuild

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ let persons = [
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
+app.use(express.static("build"));
 morgan.token("body", function (req, res) {
   return JSON.stringify(req.body);
 });
